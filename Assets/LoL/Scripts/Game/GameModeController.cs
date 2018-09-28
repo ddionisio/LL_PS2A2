e7b98939f7409cb17736eac42ff0dbbbd8d86fa4
ddionisio <garapata@gmail.com>
2018-09-28 12:13:25 -0400

    initial project setup

---
+++ Assets/LoL/Scripts/Game/GameModeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameModeController<T> : M8.SingletonBehaviour<T> where T : MonoBehaviour {
+    [Header("Mode Info")]
+    public GameMode mode;
+    public GameModeSignal signalModeChanged;
+
+    protected override void OnInstanceInit() {
+        //ensure UIRoot exists
+        if(!UIRoot.isInstantiated)
+            UIRoot.Reinstantiate();
+    }
+
+    protected override void OnInstanceDeinit() {
+        //invalidate mode
+        if(signalModeChanged)
+            signalModeChanged.Invoke(null);
+    }
+
+    protected virtual IEnumerator Start() {
+        //if(M8.SceneManager.isInstantiated) {
+            do {
+                yield return null;
+            } while(M8.SceneManager.instance.isLoading);
+       // }
+
+        if(signalModeChanged)
+            signalModeChanged.Invoke(mode);
+    }
+}
---
+++ Assets/LoL/Scripts/Game/GameStart.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStart : MonoBehaviour {
+    public static bool isStarted = false;
+
+    public GameObject loadingGO;
+    public GameObject readyGO;
+
+    public GameObject titleGO;
+    public Text titleText;
+    [M8.Localize]
+    public string titleStringRef;
+
+    public string musicPath;
+
+    void Awake() {
+        if(loadingGO) loadingGO.SetActive(true);
+        if(readyGO) readyGO.SetActive(false);
+        if(titleGO) titleGO.SetActive(false);
+    }
+
+    IEnumerator Start () {
+        //hide other stuff
+
+        yield return null;
+
+        //wait for scene to load
+        while(M8.SceneManager.instance.isLoading)
+            yield return null;
+        
+        //wait for LoL to load/initialize
+        while(!LoLManager.instance.isReady)
+            yield return null;
+        
+        //start title
+        if(titleText) titleText.text = LoLLocalize.Get(titleStringRef);
+        if(titleGO) titleGO.SetActive(true);
+
+        //show other stuff
+
+        if(loadingGO) loadingGO.SetActive(false);
+        if(readyGO) readyGO.SetActive(true);
+
+        //play music
+        if(!string.IsNullOrEmpty(musicPath))
+            LoLManager.instance.PlaySound(musicPath, true, true);
+
+        isStarted = true;
+    }
+}
