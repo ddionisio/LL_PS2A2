8779d4c2d4f0f4070e472e4a5ca7c4012b6751e9
ddionisio <garapata@gmail.com>
2018-10-03 16:36:56 -0400

    placeholder dialog, prep intro

---
+++ Assets/LoL/Scripts/UI/Modals/ModalDialogParam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Use this as a convenience to open dialog and store its values
+/// </summary>
+[System.Serializable]
+public class ModalDialogParam {
+    public string modal = "dialog";
+
+    public Sprite portrait;
+
+    [M8.Localize]
+    public string nameTextRef;
+    [M8.Localize]
+    public string dialogTextRef;
+
+    public void Open(System.Action nextCallback) {
+        ModalDialog.OpenApplyPortrait(modal, portrait, nameTextRef, dialogTextRef, nextCallback);
+    }
+
+    public void Close() {
+        if(M8.UIModal.Manager.instance.ModalIsInStack(modal))
+            M8.UIModal.Manager.instance.ModalCloseUpTo(modal, true);
+    }
+}
