5b9f7a9c1c810e1046d80ae3fee795233906c727
ddionisio <garapata@gmail.com>
2018-10-25 12:13:44 -0400

    some act 3-1 adjustments, act 3-2

---
+++ Assets/Scripts/Game/ActForceGeneralController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Generic controller for the forces section
+/// </summary>
+public class ActForceGeneralController : ActForceController {
+
+}
---
+++ Assets/Scripts/Game/SignalPlayAnimationToNext.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalPlayAnimationToNext : MonoBehaviour {
+    public Rigidbody2D body; //turn off simulator
+
+    [Header("Animation")]
+    public M8.Animator.Animate animator;
+    [M8.Animator.TakeSelector(animatorField = "animator")]
+    public string takeReceive; //take to play on receive
+    [M8.Animator.TakeSelector(animatorField = "animator")]
+    public string takeNext; //take to play after sending next signal
+
+    [Header("Signals")]
+    public M8.Signal signalReceive; //signal to listen to
+    public M8.Signal signalNext; //signal after animation
+
+    void OnDestroy() {
+        signalReceive.callback -= OnSignalReceive;
+    }
+
+    void Awake() {
+        signalReceive.callback += OnSignalReceive;
+    }
+
+    void OnSignalReceive() {
+        StartCoroutine(DoReceive());
+    }
+
+    IEnumerator DoReceive() {
+        if(body) body.simulated = false;
+
+        if(animator && !string.IsNullOrEmpty(takeReceive)) {
+            animator.Play(takeReceive);
+            while(animator.isPlaying)
+                yield return null;
+        }
+
+        if(signalNext != null) signalNext.Invoke();
+
+        if(animator && !string.IsNullOrEmpty(takeNext))
+            animator.Play(takeNext);
+    }
+}
