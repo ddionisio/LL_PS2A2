8b1a7316308ffe174ba794f914bef2815aa8a11b
ddionisio <garapata@gmail.com>
2018-10-31 19:55:48 -0400

    act 1-1 crate, initial knight, watchtower

---
+++ Assets/Scripts/UI/Widgets/MassDisplayWidget.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MassDisplayWidget : MonoBehaviour {
+    public Text label;
+    public string format = "{0:N0} Kg";
+    public Rigidbody2D body;
+
+    void OnEnable() {
+        label.text = string.Format(format, body.mass);
+    }
+}
