843181c2131a8095e808c64307454e96ff281d52
ddionisio <garapata@gmail.com>
2018-11-19 21:53:47 -0500

    art stuff for act 2-2

---
+++ Assets/Scripts/Game/TransformUpOrient.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformUpOrient : MonoBehaviour {
+    public float ofs;
+    public float scale = 1f;
+
+    public void Apply(float a) {
+        transform.up = M8.MathUtil.RotateAngle(Vector2.up, ofs + (a * scale));
+    }
+}
