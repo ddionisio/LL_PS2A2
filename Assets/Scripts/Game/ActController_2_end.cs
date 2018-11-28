0db4076c565ed524359d6fd1ee78cfedc058e4bf
ddionisio <garapata@gmail.com>
2018-11-27 21:33:47 -0500

    initial end scene, act 2 end

---
+++ Assets/Scripts/Game/ActController_2_end.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActController_2_end : GameModeController<ActController_2_end> {
+
+    protected override void OnInstanceInit() {
+        base.OnInstanceInit();
+    }
+
+    protected override IEnumerator Start() {
+        yield return base.Start();
+
+        //wait a bit
+
+        //dialog
+
+        //wait a bit
+
+        //next level
+    }
+}
---
+++ Assets/Scripts/Game/EndController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndController : GameModeController<EndController> {
+
+    protected override void OnInstanceInit() {
+        base.OnInstanceInit();
+    }
+
+    protected override IEnumerator Start() {
+        yield return base.Start();
+
+        //wait a bit
+
+        //show complete
+    }
+}
