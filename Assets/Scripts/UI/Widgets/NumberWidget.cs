aff6a1cb01125b078c2d943c2d8828af0c9f599b
David Dionisio <ddionisio@renegadeware.com>
2022-11-30 11:17:41 -0500

    extra stuff to act 3 end

---
+++ Assets/Scripts/UI/Widgets/NumberWidget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWidget : MonoBehaviour {
+    public Text label;
+    public string format = "{0}";
+
+    public float number {
+        get { return mNumber; }
+        set {
+            if(mNumber != value) {
+                mNumber = value;
+                if(label)
+                    label.text = string.Format(format, mNumber);
+            }
+        }
+    }
+
+    private float mNumber = 0f;
+
+    void Awake() {
+        if(!label)
+            label = GetComponent<Text>();
+    }
+}
